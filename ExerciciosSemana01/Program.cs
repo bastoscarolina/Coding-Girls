6bb1143e7f32a703cec775bcd87a2d3732402c74
Carolina <almeida.carolinabastos@gmail.com>
2022-06-06 22:19:35 -0300

    exercicio 1 da aula 01 da semana 01

---
+++ ExerciciosSemana01/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Aula 01
+
+//Exercício 01
+Console.WriteLine("Nome: Carolina Bastos Almeida");
+Console.WriteLine("Endereço: Rua ABC do XPTO,999");
+Console.WriteLine("CEP: 99.999-999");
+Console.WriteLine("Telefone: (99) 9 9999-9999");
+//Exercício 02
+
+//Exercício 03
+
+//Exercício 04
