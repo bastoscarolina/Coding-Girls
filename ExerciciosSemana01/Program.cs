55b41033d3afe26eabe35b063fc516bd7e76b0f6
Carolina <almeida.carolinabastos@gmail.com>
2022-06-06 23:12:41 -0300

    Exercicios da aula 01 da semana 01

--- ExerciciosSemana01/Program.cs
+++ ExerciciosSemana01/Program.cs
@@ -8,7 +8,21 @@ Console.WriteLine("Endereço: Rua ABC do XPTO,999");
 Console.WriteLine("CEP: 99.999-999");
 Console.WriteLine("Telefone: (99) 9 9999-9999");
 //Exercício 02
-
+string nome = "Grace Hopper";
+string formacao = "Analista de Sistemas";
+string contribuicao = "Criou a Linguagem Comum Orientada para Negócios (COBOL, na sigla em inglês)";
+Console.WriteLine($"Nome: {nome}");
+Console.WriteLine($"Formação: {formacao}");
+Console.WriteLine($"Contribuição feita: {contribuicao}");
 //Exercício 03
-
+string refraoMusica ="The show must go on, the show must go on, inside my heart is breaking, my makeup may be flaking, but my smile, still, stays on";
+string compositor = "Freddie Mercury, Roger Meddows-Taylor, Brian May e John Deacon";
+string generoMusical = "Pop Rock/French Indie";
+Console.WriteLine(refraoMusica);
+Console.WriteLine($"Compositor(es): {compositor}");
+Console.WriteLine($"Gênero: {generoMusical}");
 //Exercício 04
+string mensagemIncentivo = "Eu levanto minha voz - não para que eu possa gritar, mas para que aqueles Sem uma voz pode ser ouvida ... não podemos ter sucesso quando metade de nós são retidos. - Malala";
+Console.WriteLine(mensagemIncentivo);
+
+//Aula 02
