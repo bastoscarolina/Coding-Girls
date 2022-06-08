1a331b8959266104c03193f25da69e85644bfb49
Carolina <almeida.carolinabastos@gmail.com>
2022-06-07 22:37:52 -0300

    exercicios aula 02 da semana 01

--- ExerciciosSemana01/Program.cs
+++ ExerciciosSemana01/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
+/*
 //Aula 01
 
 //Exercício 01
@@ -24,5 +24,47 @@ Console.WriteLine($"Gênero: {generoMusical}");
 //Exercício 04
 string mensagemIncentivo = "Eu levanto minha voz - não para que eu possa gritar, mas para que aqueles Sem uma voz pode ser ouvida ... não podemos ter sucesso quando metade de nós são retidos. - Malala";
 Console.WriteLine(mensagemIncentivo);
+*/
 
 //Aula 02
+
+//Exercício 01
+Console.WriteLine("Qual seu ano de nascimento?");
+int anoNascimento = int.Parse(Console.ReadLine());
+int anoAtual = 2022;
+int idade = anoAtual - anoNascimento;
+Console.WriteLine($"Ao final de {anoAtual} você terá {idade} anos");
+//Exercício 02
+int[] valores = new int[10];
+int soma = 0;
+for(int i =0; i<valores.Length;i++){
+    Console.WriteLine($"Insira o valores na posição {i}");
+    valores[i] = int.Parse(Console.ReadLine());
+    soma += valores[i];
+}
+Console.WriteLine($"A soma dos valores deu {soma}");
+//Exercício 03
+Console.WriteLine("Quantas horas o funcionário trabalhou?");
+float horasTrabalhadas = float.Parse(Console.ReadLine());
+Console.WriteLine("Qual é o salário por hora desse funcionário?");
+float salarioHora = float.Parse(Console.ReadLine());
+float salarioFinal = horasTrabalhadas * salarioHora;
+Console.WriteLine($"O funcionário receberá R${salarioFinal:F2} por {horasTrabalhadas} horas trabalhadas");
+//Exercício 04
+Console.WriteLine("Qual a idade a seu convertida?");
+int anos = int.Parse(Console.ReadLine());
+int meses = anos * 12;
+int dias = meses * 30;
+Console.WriteLine($"O usuário tem {anos} anos ou {meses} meses ou {dias} dias de idade");
+
+//Exercício 05
+Console.WriteLine("Digite o valor a ser convertido:");
+float real = float.Parse(Console.ReadLine());
+float dolar = real / 4.87f;
+float euro = real / 5.21f;
+float libra = real / 6.12f;
+float dolarCanadense = real / 3.88f;
+float pesoArgentino = real / 0.04f;
+float pesoChileno = real / 0.0059f;
+
+Console.WriteLine($"O valor em real corresponde a :\nUS$ {dolar:F2}\nE$ {euro:F2}\nL$ {libra:F2}\nC$ {dolarCanadense:F2}\nARS$ {pesoArgentino:F2}\nCLP$ {pesoChileno:F2}");
